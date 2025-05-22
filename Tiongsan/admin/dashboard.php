9123c142d0a46c69651175667cc1c36977747676
Vea-Capistrano <veacapistrano@gmail.com>
2025-05-22 16:07:21 +0800

    Add files via upload

--- Tiongsan/admin/dashboard.php
+++ Tiongsan/admin/dashboard.php
@@ -1,11 +1,51 @@
+<?php
+    if($_SERVER['REQUEST_METHOD'] == "POST") {
+        $conn = mysqli_connect("localhost", "batch1", "batch1", "db_tiongsan", 3307);
+        
+        $coursename = $_POST['coursename'];
+        $coursedesc = $_POST['coursedesc'];
+        $coursecat = $_POST['coursecat'];
+
+        $conn = mysqli_connect("localhost", "batch1", "batch1", "db_tiongsan", 3307);
+
+        $sql = "INSERT INTO `tbl_courses`(`course_name`, `course_description`, `category`) VALUES ('$coursename', '$coursedesc', '$coursecat')";
+        $result = $conn->query($sql);
+        if($result){
+            echo "Course Created Successfully";
+        }
+        
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Admin Dashboard</h1>
+    <div class="container">
+        <header class="row"></header>
+        <main class="row">
+            <h1>Create Course</h1>
+            <form action="" method="POST">
+                <label for="coursename">Course Name</label>
+                <input type="text" name="coursename" class="form-control">
+
+                <label for="coursedesc">Course Description</label>
+                <textarea name="coursedesc" id="coursedesc" class="form-control"></textarea>
+
+                <label for="coursecat">Course Category</label>
+                <input type="text" name="coursecat" class="form-control">
+
+                <input type="submit" value="Create Course" class="btn btn-primary mt-3">
+            </form>
+        </main>
+        <footer class="row"></footer>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 </html>
--- Tiongsan/student/dashboard.php
+++ Tiongsan/student/dashboard.php
@@ -1,11 +1,59 @@
+<?php
+    $conn = mysqli_connect("localhost", "batch1", "batch1", "db_tiongsan", 3307);
+
+    $sql = "SELECT * FROM `tbl_courses`";
+    $result = $conn->query($sql);
+    $courses = [];
+    if($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()){
+            $courses[] = $row;
+            
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Student Dashboard</h1>
+    <div class="container">
+        <header class="row bg-primary">
+            <div class="col-4">
+            <nav class="navbar bg-body-tertiary">
+                <div class="container">
+                    <a class="navbar-brand" href="#">
+                        <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Bootstrap" width="30" height="24">
+                    </a>
+                </div>
+            </nav>
+            </div>
+            <div class="col-4">col2</div>
+            <div class="col-4">col3</div>
+        </header>
+        <main class="row p-3">
+            <?php
+                foreach($courses as $course) {
+            ?>
+              <div class="card m-2" style="width: 18rem;">
+                <img src="..." class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $course["course_name"]; ?></h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                    <a href="#" class="btn btn-primary">Enroll</a>
+                </div>
+              </div>
+            <?php  
+                }
+            ?>
+        </main>
+        <footer class="row bg-primary">footer</footer>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 </html>
